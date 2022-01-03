3664feb0850b76cb96016494f89463967122cd57
Ayelen85 <scolesjulian1@gmail.com>
2022-01-03 17:15:34 -0300

    Se agrega el proyecto

---
+++ src/helpers/auth.js
@@ -0,0 +1,13 @@
+const helpers = {};
+
+// export const isAuthenticated = (req, res, next) => {
+helpers.isAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    req.flash('error_msg', 'Not Authorized');
+    res.redirect('/users/signin');
+  };
+
+  module.exports = helpers;
+  
